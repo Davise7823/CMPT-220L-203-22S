8cb4a9d86e4a85699f3151099ffc6f7a7bcd4571
Davise7823 <Elijah.Davis1@marist.edu>
2022-02-17 19:39:42 -0500

    Lab 4

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -15,9 +15,43 @@ public class Main {
      * - Structuring solutions
      */
     public static void main(String[] args) {
-        // Give me an example of you using switch case.
+        // Give me an example of you using switch case
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
         // Give me an example of you using a for loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
         // Give me an example of you using a while loop
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
 
@@ -26,18 +60,46 @@ public class Main {
      * For example if given the numbers 2 and 3. The math that the function should do is 2^3
      * and should print out or return 8. Print the output.
      */
+    int x = 2;
+    int y = 3;
+
+    double exponential(int x , int y){
+        double sol = Math.pow(x,y);
+        return sol;
+    }
 
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
+    int [] array = {2,3,5,2,3,6,7,8,2};
+
+    int amount(int[] arr){
+        int j = 0;
+        for(int num : arr) {
+            int i = arr[num];
+            if (num == i) {
+                j++;
+            }
+        }
+        return j;
+    }
+
 
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
+
+    int summation(int[] arr ){
+        int n = 0;
+        for(int num : arr ){
+            n = n + num;
+        }
+        return n;
+    }
 
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
@@ -50,6 +112,18 @@ public class Main {
      *
      * Once that’s done print or return the answer. Print the output
      */
+
+    int a = 4;
+    int b = 5;
+
+    double pythag(int a, int b){
+            double ans2 = Math.pow(a,2) + Math.pow(b,2);
+            double ans = Math.sqrt(ans2);
+            return ans;
+        }
+
+
+
 
     int addition(int a, int b) {
         int answer = a + b;
